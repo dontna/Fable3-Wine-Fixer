cda1007efa118200ab5e7413c094420cc5c22250
Peter <peterholland38@googlemail.com>
2023-11-13 17:44:10 +0000

    made script automatically find game directory

--- fable3_fixer.sh
+++ fable3_fixer.sh
@@ -2,32 +2,33 @@
 
 # Argument Checks
 if [[ $1 == "--help" || $1 == "-h" ]]; then
-	echo "Script usage: $0 \"[PATH_TO_PREFIX]\" \"[PATH_TO_GAME_DIRECTORY]\""
+	echo "Script usage: $0 \"[PATH_TO_PREFIX]\""
 	echo "Please refer to the 'help.txt' document, if you need more information."
-	exit 0
+	exit 1
 fi
 
-if [[ -z "$1" || -z "$2" ]]; then
+if [[ -z "$1" ]]; then
 	echo "Not enough aguments passed"
-	exit 0
+	exit 1
 fi
 
 # Directory Checks
 if [ "$(ls -A $1)" ]; then
 	echo "\"$1\" is not empty. Please create a new folder and point the script to that."
-	exit 0
+	exit 1
 fi
 
-if ! [[ $(ls "$2" -1 | grep FableLauncher.exe) ]]; then
-	echo "Cannot find 'FableLauncher.exe' in \"$2\""
-	echo "Are you sure this is your Fable 3 game directory?"
-	exit 0
-fi
 	
 ### START OF THE ACTUAL SCRIPT ###
 
 PREFIX_PATH=$1
-GAME_DIRECTORY_PATH=$2
+GAME_DIRECTORY_PATH=$(find / type -d -path "*/steamapps/common/Fable 3" -print -quit 2> /dev/null)
+
+if ! [[ $(ls "$GAME_DIRECTORY_PATH" -1 | grep FableLauncher.exe) ]]; then
+	echo "Cannot find 'FableLauncher.exe' in \"$GAME_DIRECTORY_PATH\""
+	echo "Are you sure this is your Fable 3 game directory?"
+	exit 1
+fi
 
 # Create Wine Bottle
 WINEARCH=win32 WINEPREFIX="$PREFIX_PATH" winepath
@@ -40,8 +41,8 @@ tar -xvzf dxvk-1.10.3.tar.gz
 rm "dxvk-1.10.3.tar.gz"
 
 # Download GFWL Bypass
-mkdir GFWLRemover/ 
-cd GFWLRemover/
+mkdir $(pwd)/GFWLRemover/ 
+cd $(pwd)/GFWLRemover/
 wget "https://www.dropbox.com/sh/l1yugur7zvzvwvz/AACZ-HjSYJibUS0GDW9eJZA0a/GFWL%20Disabler.zip" -O $(pwd)/GFWL_Remover.zip
 unzip GFWL_Remover.zip
 rm *.zip
@@ -49,7 +50,8 @@ rm *.zip
 # Install Vulkan
 cd ../dxvk-1.10.3/
 echo "Running DXVK Vulkan Install Script..."
-WINEPREFIX="$PREFIX_PATH" ./setup_dxvk.sh install
+export WINEPREFIX="$PREFIX_PATH" 
+./setup_dxvk.sh install
 mv --force d3d9.dll dxgi.dll "$GAME_DIRECTORY_PATH"
 
 # Install GWFL Bypass
@@ -57,8 +59,7 @@ cd ../GFWLRemover/
 mv --force xlive.dll xlive.ini "$GAME_DIRECTORY_PATH"
 
 # Finished 
-clear
-echo "Script is complete, please open the game using the following command. Also please read 'help.txt' to see any additional fixes you may need."
+echo -e "\n\nScript is complete, please open the game using the following command. Also please read 'help.txt' to see any additional fixes you may need."
 echo "Also after copying the following command, you will have to give your CD Key. This can be found in Steam."
 echo "Copy the following command to start the game: "
 echo ""
